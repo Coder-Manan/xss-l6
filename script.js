c7c3773d6bfab20aa236f6b7849bf8ffc0e4a0a6
Manan Garg <92792271+Coder-Manan@users.noreply.github.com>
2022-07-22 17:58:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-alert("hello");
+/* the content of file is from https://xss.rocks/xss.js by me for test purposes*/
+document.write ("This is remote text via xss.js located at xss.rocks " + document.cookie);
+alert ("This is remote text via xss.js located at xss.rocks " + document.cookie);
